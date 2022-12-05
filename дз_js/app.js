17733cb540419badca072de16a608a49c4528ff8
NastiaKuzyk <117673786+NastiaKuzyk@users.noreply.github.com>
2022-12-06 00:17:17 +0200

    Add files via upload

---
+++ дз_js/app.js
@@ -0,0 +1,24 @@
+//Homework_1
+let city = "Київ";
+city = "Львів";
+let adress;
+city = adress;
+alert(adress);
+
+//Homework_2
+let n = 5;
+let result = n *** 3;
+console.log(result);
+
+//Homework_3
+let nk = 4;
+let mk = 6;
+let n = 5.25;
+let m = 3.50;
+let f = nk * n;
+let g = mk * m;
+document.write = (f + g);
+
+
+
+
